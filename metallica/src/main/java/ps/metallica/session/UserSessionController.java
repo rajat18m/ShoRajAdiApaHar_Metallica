0bdb3bf2b229b6d4f626723ee8c1becc2702599f
Shourojit Dutt <shodutt@publicisgroupe.net>
2019-04-23 16:07:00 +0530

    Edited Session Controller for Party

--- metallica/src/main/java/ps/metallica/session/UserSessionController.java
+++ metallica/src/main/java/ps/metallica/session/UserSessionController.java
@@ -1,10 +1,13 @@
 package ps.metallica.session;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import ps.metallica.entities.Party;
 
@@ -19,14 +22,31 @@ public class UserSessionController {
 	 * @param session
 	 * @return
 	 */
-	@GetMapping("/api/user")
-	public String userAuth(Model model, HttpSession session) {
+	@GetMapping("/")
+	public String userLogin(Model model, HttpSession session) {
 		Party party = (Party) session.getAttribute("PARTY");
 		if(party == null) {
-			return "failed";
+			return "redirect:/login";
 		}
 		model.addAttribute("currentParty", party);
-		return "login";
+		return "redirect:/dashboard";
+	}
+	
+	@PostMapping("/api/user")
+	public String userRegistration(@RequestParam("email") String email, @RequestParam("password") String password,
+			@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName, HttpServletRequest request) {
+		Party party = (Party) request.getSession().getAttribute("PARTY");
+		if(party == null) {
+			party = new Party(password, firstName, lastName, email);
+		}
+		request.getSession().setAttribute("PARTY", party);
+		return "redirect:/";
+	}
+	
+	@PostMapping("/destroy")
+	public String destroySession(HttpServletRequest request) {
+		request.getSession().invalidate();
+		return "redirect:/";
 	}
 	
 }
