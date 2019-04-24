724712b74ec8cf18d4a6aaa9924894627daffcce
Shourojit Dutt <shodutt@publicisgroupe.net>
2019-04-24 15:24:53 +0530

    Added NavBar component and created style sheet

---
+++ frontend/src/app/navbar.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-navbar',
+    template: `
+    <nav class="navbar navbar-expand-lg navbar-light metallica-border-left">
+  <a class="navbar-brand robo-condensed" href="#">Metallica</a>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link eczarified metallica-border-sides" href="/trades">Trades <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link eczarified metallica-border-sides" href="/transfers">Transfers</a>
+      </li>
+    </ul>
+    <span class="navbar-text robo-condensed metallica-border-bot far-end">
+      {{username}}
+    </span>
+  </div>
+</nav>
+    `
+})
+
+export class NavbarComponent implements OnInit {
+
+  username : string
+    constructor() { }
+
+    ngOnInit() {
+      this.username = "Lorem"
+     }
+}
+
